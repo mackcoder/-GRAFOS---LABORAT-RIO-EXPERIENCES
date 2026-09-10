bb197f6281ea6ff132d10cceb96245d78ee2bd84
Naoto Ushizaki <naoto.ushizaki@gmail.com>
2026-09-09 23:23:34 -0300

    Implement file reading and degree calculation in TGrafo_ND
    Added methods to read graph data from a file, calculate vertex degree, and improved edge insertion and removal logic.

--- src/TGrafo_ND.cpp
+++ src/TGrafo_ND.cpp
@@ -36,6 +36,29 @@ TGrafo_ND::~TGrafo_ND(){
 	std::cout << "espaco liberado";
 }
 
+// ⚠️EXERCICIO 7 - RELATORIO
+void TGrafo_ND::readfile(const char* grafo_example){
+    std::ifstream file(grafo_example);
+    if(!file.is_open()){
+        std::cout << "ERROR - Unable to access file" << std::endl;
+        return;
+    }
+
+    int V;
+    int A;
+    file >> V;
+    file >> A;
+
+    int a1, a2;
+    for(int y = 0; y < A; y++){
+        file >> a1 >> a2;
+        NDinsereA(a1, a2);
+    }
+    file.close();
+}
+
+// ⚠️EXERCICIO 8 (Funções ND) - RELATORIO
+
 // Insere uma aresta no Grafo tal que
 // v  adjacente a w
 void TGrafo_ND::NDinsereA(int v, int w){
@@ -51,13 +74,23 @@ void TGrafo_ND::NDinsereA(int v, int w){
 // remove uma aresta v->w do Grafo
 void TGrafo_ND::NDremoveA(int v, int w){
     // testa se temos a aresta
-    if(adj[v][w] == 1 && adj[w][v] = 1){
+    if(adj[v][w] == 1 && adj[w][v] == 1){
         adj[v][w] = 0;
         adj[w][v] = 0;
         m--; // atualiza qtd arestas
     }
 }
 
+// ⚠️EXERCICIO 9 - RELATORIO
+int TGrafo_ND::degree(int v){
+    int ingrau_nd = 0;
+    for(int b = 0; b < n; b++){
+        if(adj[v][b] == 1){
+            ingrau_nd++;
+        }
+    }
+    return ingrau_nd;
+}
 
 void TGrafo_ND::NDshow(){
     std::cout << "n: " << n << std::endl;
@@ -69,5 +102,10 @@ void TGrafo_ND::NDshow(){
                 std::cout << "Adj[" << i<< "," << w << "]= 1" << " ";
             else std::cout << "Adj[" << i<< "," << w << "]= 0" << " ";
     }
+    std::cout << "\nRESPOSTA 9)\n";
+    for(int index = 0; index < n; index++){
+        std::cout << "Grau do vertice " << index << ": " << degree(index) << std::endl;
+    }
+    
     std::cout << "\nfim da impressao do grafo." << std::endl;
 }
