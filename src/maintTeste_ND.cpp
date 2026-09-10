5f81f8278e23e478a4d2ad5cdc55fe9aff56b9fe
Naoto Ushizaki <naoto.ushizaki@gmail.com>
2026-09-09 23:24:39 -0300

    Add main function to initialize and display graph

---
+++ src/maintTeste_ND.cpp
@@ -0,0 +1,21 @@
+/*
+- Andre Doerner Duarte - 10427938
+- Matheus Leonardo Cardoso Kroeff - 10426434
+- Naoto Ushizaki - 10437445
+*/
+
+#include "TGrafo_ND.h"
+
+int main(){
+    //  chama o construtor para criar um grafo 4x4
+    TGrafo_ND g2(6);
+
+    //---------------------------------------------//
+
+    g2.readfile("grafo_example.txt");
+
+    g2.NDshow(); 
+
+
+    return 0;
+}
